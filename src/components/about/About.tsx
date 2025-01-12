d7f861a7bb86ebed47b2325d892084c6deee8057
HaroruDa3 <harol.kock34@gmail.com>
2025-01-11 22:52:51 -0600

    adaptabilidad en movil

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,26 +1,31 @@
-import './about.css'
+import "./about.css";
 export const About = () => {
   return (
-    <section id='about-us' className='about-us'>
-        <div className='p-5 pb-1'>
+    <section id="about-us" className="about-us">
+      <div className="p-5 pb-1">
         <img src="/patriotTokenCircle.png" alt="" />
-        
+
         <h1>Patriot Token: </h1>
         <h1>Leading the Dawn of a New Era</h1>
-        <h2 className='info-about'>The memecoin that unites progress, leadership and community in a global movement.</h2>
-        <h2 className='join-movement'>Launch January 20th, 12M Washington D.C. time</h2>
-        
-        </div>
-       
-        <div className='banner-news'>
-        <div className='news-1'>
-            <p>BREAKING NEWS</p>
-        </div>
-        <div className='news-2'>
-            <p>Donald Trump IS THE NEW PRESIDENT AND WILL LEAD US INTO THE BULLRUN </p>
-        </div>
-        </div>
+        <h2 className="info-about">
+          The memecoin that unites progress, leadership and community in a
+          global movement.
+        </h2>
+        <h2 className="join-movement">
+          Launch January 20th, 12M Washington D.C. time
+        </h2>
+      </div>
 
+      <div className="banner-news">
+        <div className="news-1">
+          <p>BREAKING NEWS</p>
+        </div>
+        <div className="news-2">
+          <p>
+            Donald Trump IS THE NEW PRESIDENT AND WILL LEAD US INTO THE BULLRUN{" "}
+          </p>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
